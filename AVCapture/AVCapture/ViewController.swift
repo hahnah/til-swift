2ac2f0b12d3639856b08d627747494df4a06e54c
hahnah <superhahnah@gmail.com>
2019-01-31 21:27:14 +0900

    Initial commit; Basic implements

---
+++ AVCapture/AVCapture/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  AVCapture
+//
+//  Created by Natsuki HARAI on 2019/01/31.
+//  Copyright © 2019 hahnah. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+    let fileOutput = AVCaptureMovieFileOutput()
+    
+    var recordButton: UIButton!
+    var isRecording = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .black
+        self.setUpCamera()
+    }
+    
+    func setUpCamera() {
+        let videoDevice: AVCaptureDevice? = AVCaptureDevice.default(for: AVMediaType.video)
+        let audioDevice: AVCaptureDevice? = AVCaptureDevice.default(for: AVMediaType.audio)
+        let captureSession: AVCaptureSession = AVCaptureSession()
+        
+        // video input setting
+        let videoInput: AVCaptureDeviceInput = try! AVCaptureDeviceInput(device: videoDevice!)
+        captureSession.addInput(videoInput)
+        
+        // audio input setting
+        let audioInput = try! AVCaptureDeviceInput(device: audioDevice!)
+        captureSession.addInput(audioInput)
+        
+        // max duration setting
+        self.fileOutput.maxRecordedDuration = CMTimeMake(value: 60, timescale: 1)
+        captureSession.addOutput(fileOutput)
+        
+        captureSession.startRunning()
+        
+        // video preview layer
+        let videoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoLayer.frame = self.view.bounds
+        videoLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.view.layer.addSublayer(videoLayer)
+        
+        // recording button
+        self.recordButton = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
+        self.recordButton.backgroundColor = UIColor.gray
+        self.recordButton.layer.masksToBounds = true
+        self.recordButton.setTitle("Record", for: .normal)
+        self.recordButton.layer.cornerRadius = 20
+        self.recordButton.layer.position = CGPoint(x: self.view.bounds.width / 2, y:self.view.bounds.height - 100)
+        self.recordButton.addTarget(self, action: #selector(self.onClickRecordButton(sender:)), for: .touchUpInside)
+        self.view.addSubview(recordButton)
+    }
+    
+    @objc func onClickRecordButton(sender: UIButton) {
+        if !self.isRecording {
+            // start recording
+            let tempDirectory: URL = URL(fileURLWithPath: NSTemporaryDirectory())
+            let fileURL: URL = tempDirectory.appendingPathComponent("mytemp1.mov")
+            fileOutput.startRecording(to: fileURL, recordingDelegate: self)
+            
+            self.isRecording = true
+            self.recordButton.backgroundColor = .red
+            self.recordButton.setTitle("●Recording", for: .normal)
+        } else {
+            // stop recording
+            fileOutput.stopRecording()
+            
+            self.isRecording = false
+            self.recordButton.backgroundColor = .gray
+            self.recordButton.setTitle("Record", for: .normal)
+        }
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        // show alert
+        let alert: UIAlertController = UIAlertController(title: "Saved!", message: outputFileURL.absoluteString, preferredStyle:  .alert)
+        let okAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
